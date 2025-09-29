adcdaa4a8287b03c0994b8ff355b1218e55b3407
Sitoosito8 <a20000lex@gmail.com>
2025-09-29 20:29:39 +0200

    Subir cambios sobre la practica de Ruben

--- DWEC(IVAN)/SA2/Práctica 1.2_ frase/scripts/main.js
+++ DWEC(IVAN)/SA2/Práctica 1.2_ frase/scripts/main.js
@@ -21,16 +21,3 @@ function mostrarDatos() {
     console.log(palindrome(frase) ? "Sí es un palídermo" : "No es un palídermo");
 }
 mostrarDatos();
-
-function mostrarDatos() {
-
-    let frase = prompt("Frase:");
-
-    console.log(letters(frase) + " letras y " + words(frase) + " palabras");
-    console.log(upperString(frase));
-    console.log(titleString(frase));
-    console.log(backwardsLetters(frase));
-    console.log(backwardsWords(frase));
-    console.log(palindrome(frase) ? "Sí es un palídermo" : "No es un palídermo");
-}
-mostrarDatos();

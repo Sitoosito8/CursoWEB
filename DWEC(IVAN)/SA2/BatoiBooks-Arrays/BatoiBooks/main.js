56c382bc79e35a8127cb819b192e677d07168cc6
Sitoosito8 <a20000lex@gmail.com>
2025-10-08 19:23:19 +0200

    Subir cambios relacionados con la actividad de ivan de BatoiBooks

--- DWEC(IVAN)/SA2/BatoiBooks-Arrays/BatoiBooks/main.js
+++ DWEC(IVAN)/SA2/BatoiBooks-Arrays/BatoiBooks/main.js
@@ -3,4 +3,19 @@ import * as functions from './src/functions';
 
 
 console.log(functions.getBookById(data.books,1));
+console.log(functions.getBookIndexById(data.books,6));
+console.log(functions.bookExists(data.books,3,5021));
+console.log(functions.booksFromUser(data.books,4));
+console.log(functions.booksFromModule(data.books,"5025"));
+console.log(functions.booksCheeperThan(data.books,60))
+console.log(functions.booksWithStatus(data.books, "good"));
+console.log(functions.averagePriceOfBooks(data.books));
+console.log(functions.booksOfTypeNote(data.books));
+console.log(functions.booksNotSold(data.books));
+console.log(functions.incrementPriceOfbooks(data.books,10));
+console.log(functions.getUserById(data.users,1));
+console.log(functions.getUserIndexById(data.users,1));
+console.log(functions.getUserByNickName(data.users,"nick"));
+console.log(functions.getModuleByCode(data.modules,"5025"));
+
 console.log("Aplicación BatoiBooks cargada correctamente");
--- DWEC(IVAN)/SA2/BatoiBooks-Arrays/BatoiBooks/src/functions.js
+++ DWEC(IVAN)/SA2/BatoiBooks-Arrays/BatoiBooks/src/functions.js
@@ -1,27 +1,91 @@
 
 'use strict'
 
-function getBookById(books,bookId){
-   const book = books.find(book => book.id == bookId);
-   if(!book){
+function getBookById(books, bookId) {
+  if (books.length === 0) {
+    console.error("El array de libreso está vacio");
+    return null;
+  }
+  let book = books.find(book => book.id === bookId);
+  if (!book) {
     console.error("No existe esa id en el array de libros");
     return null
-   }
-   return book;
+  }
+  return book;
 
 }
 
-function getBookIndexById(books,bookId){
-    const posicion = books.findIndex(book => book.id==bookId);
-    return 
+function getBookIndexById(books, bookId) {
+  if (books.length === 0) {
+    console.error("El array de libreso está vacio");
+    return null;
+  }
+  const posicion = books.findIndex((book) => book.id === bookId);
+
+  if (posicion === -1) {
+    console.error("El valo que está buscando no existe.")
+    return null
+  }
+  return posicion;
+}
+
+function bookExists(books, userId, moduleCode) {
+  let comprobante = books.some((book) => book.userId === userId && book.moduleCode == moduleCode);
+  if (!comprobante) {
+    console.error("No se han pasado los parametros correctos.");
+    return false
+  }
+  return comprobante;
 
 }
 
-export{
-    getBookById,
+function booksFromUser(books, userId) {
+  let librosArray = books.filter(book => book.userId === userId);
+  return librosArray;
 }
-/*export {
-  getBookById
+
+function booksFromModule(books, moduleCode) {
+  return books.filter(book => book.moduleCode === moduleCode)
+
+}
+function booksCheeperThan(books, price) {
+  let librosArray = books.filter(book => book.price <= price)
+  return librosArray;
+}
+function booksWithStatus(books, status) {
+  let libros = books.filter(book => book.status === status);
+  return libros;
+
+}
+function averagePriceOfBooks(books){
+   let total= ((books.reduce((total, book) => total+=book.price,0))/books.length).toFixed(2) +"€";
+   return total;
+}
+function booksOfTypeNote(books){
+  return books.find(book => book.publisher === "Apunts")
+
+}
+function booksNotSold(books){
+  soldDate
+}
+function incrementPriceOfbooks(books,percentage){
+  
+}
+
+function getUserById(users,userId){
+  
+}
+function getUserIndexById(users,userId){
+  
+}
+function getUserByNickName(users,nick){
+  
+}
+function getModuleByCode(modules,modeuleCode){
+  
+}
+export {
+  getBookById,
   getBookIndexById,
   bookExists,
   booksFromUser,
@@ -36,4 +100,4 @@ export{
   getUserIndexById,
   getUserByNickName,
   getModuleByCode 
-}*/
+}
